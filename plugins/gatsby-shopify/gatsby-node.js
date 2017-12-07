8e1867655b8476b96b21f9d1f3739c4d351b7008
Darren Hebner <darren.hebner@shopify.com>
2017-12-07 11:09:06 -0500

    It sort of works

--- plugins/gatsby-shopify/gatsby-node.js
+++ plugins/gatsby-shopify/gatsby-node.js
@@ -1,11 +1,23 @@
 const fetch = require('node-fetch');
+const crypto = require('crypto');
 
 exports.sourceNodes = async ({boundActionCreators}) => {
   const {createNode} = boundActionCreators;
 
   function processDatum(datum) {
     datum.id = datum.id.toString();
-    return datum;
+    return {
+      ...datum,
+      parent: null,
+      children: [],
+      internal: {
+        type: 'product',
+        contentDigest: crypto
+          .createHash(`md5`)
+          .update(JSON.stringify(datum))
+          .digest(`hex`)
+      }
+    };
   }
 
   const data = await fetch(
